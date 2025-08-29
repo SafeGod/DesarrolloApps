ee0da561e9696e4a32bbbaa5f811b96bf0b51abe
SafeGod <nicolascabreraperdomo0@gmail.com>
2025-08-29 17:45:15 -0500

    Migracion de la tarea de la semana pasada de JS a TS

--- Clase2/src/app/app.component.ts
+++ Clase2/src/app/app.component.ts
@@ -10,11 +10,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Clase2';
-  tituloOriginal = this.title;
-  cambio = 0;
 
-  cambiartitulo(){
-    const nuevo = document.getElementById('miId') as HTMLInputElement; 
-    this.title = nuevo.value;
+  agregar() {
+    const tareaInput = document.getElementById('inputComida') as HTMLInputElement;
+    // const agregarBtn = document.getElementById('botonAgregar');
+    const listaTareas = document.getElementById('Lista') as HTMLDataListElement;
+
+    const nuevaTareaTexto = tareaInput.value;
+    if (nuevaTareaTexto === '') return;
+
+
+    const nuevaTareaLi = document.createElement('li');
+    nuevaTareaLi.textContent = nuevaTareaTexto;
+
+    //Creamos el boton de eliminar
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.style.marginLeft = '30px';
+
+    //Evento para el boton de borrar el elemento hijo 
+    botonEliminar.addEventListener('click', function () {
+      listaTareas.removeChild(nuevaTareaLi);
+    });
+
+    // Agregar el botón al li
+    nuevaTareaLi.appendChild(botonEliminar);
+    listaTareas.appendChild(nuevaTareaLi);
+
+    tareaInput.value = '';
   }
 }
